3505ec65f0afcd65ec28e29caedee88afa4b7585
Nikolas Moyseos <nikolasmoyseos@gmail.com>
2017-06-26 20:42:47 -0400

    creating dbs

---
+++ models/users.js
@@ -0,0 +1,24 @@
+const sql = require("./util/sql");
+const Sequelize = require("sequelize");
+
+module.exports = sql.define("user", {
+	id: {
+		type: Sequelize.INTEGER,
+		primaryKey: true,
+		autoIncrement: true,
+	},
+	first_name: {
+		type: Sequelize.STRING,
+		notNull: true,
+	},
+	last_name: {
+		type: Sequelize.STRING,
+		notNull: true,
+	},
+	user_name: {
+		type: Sequelize.STRING,
+		notNull: true,
+	}
+	email:
+	type: Sequelize.
+});
